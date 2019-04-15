f2f2dee2dd8b2cf61246e6ec07fc68a885ceed4b
Ben Guffey <mrben.guffey@gmail.com>
2019-04-15 15:57:39 -0500

    client commit test

---
+++ Client/src/client.java
@@ -0,0 +1,5 @@
+import java.net.Socket;					//Connects Server and Client
+import java.io.InputStream;				//Receives Data from Server
+import java.io.FileOutputStream;		//Exports Data to a file
+import java.io.BufferedOutputStream;	//Stores Data as it comes in
+
